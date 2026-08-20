b2ab394271ee38542d33d47689183eaa79c2e487
livrariainspire <livraria.app@livrariainspire.com.br>
2026-08-20 16:54:40 -0300

    config.js

--- public/config.js
+++ public/config.js
@@ -8,6 +8,6 @@
    ===================================================================== */
 
 window.__INSPIRE__ = {
-  SUPABASE_URL: "COLE_AQUI_A_URL_DO_PROJETO",
-  SUPABASE_ANON_KEY: "COLE_AQUI_A_CHAVE_ANON_PUBLIC"
+  SUPABASE_URL: "https://cotsnkzrqztumfcmlwlr.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNvdHNua3pycXp0dW1mY21sd2xyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODcyNDcwNTksImV4cCI6MjEwMjgyMzA1OX0.kS4m-JGPoiRY7rk_YLkyDV3PWwpmOlR29Opa7p08YOQ"
 };
